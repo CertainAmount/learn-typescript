ac17df754095fc5cf8fb0345819ba1776aae80f3
jeongil.yang <monday@kakao.com>
2021-07-23 16:13:08 +0900

    Init project

---
+++ why-ts/app.js
@@ -0,0 +1,36 @@
+// api url
+var url = 'https://jsonplaceholder.typicode.com/users/1';
+
+// dom
+var username = document.querySelector('#username');
+var email = document.querySelector('#email');
+var address = document.querySelector('#address');
+
+// user data
+var user = {};
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+function startApp() {
+  fetchUser()
+    .then(function (response) {
+      console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
